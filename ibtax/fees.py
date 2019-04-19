79fa058659b9fb9fc01106176f92a050a8aa64f9
Yuri Khrustalev <yuri.khrustalev@gmail.com>
2019-04-19 13:40:37 +0300

    remove duplicate

--- ibtax/fees.py
+++ ibtax/fees.py
@@ -35,36 +35,6 @@ class Fee(namedtuple('Fee', [
         return list(walk())
 
 
-class Withhold(namedtuple('Withhold', [
-    'withholding_tax', 'header', 'raw_currency', 'raw_date', 'description',
-    'raw_amount', 'code',
-])):
-    @property
-    def currency(self):
-        return self.raw_currency.upper()
-
-    @property
-    def datetime(self):
-        return datetime.strptime(self.raw_date, '%Y-%m-%d')
-
-    @property
-    def amount(self):
-        return abs(float(self.raw_amount))
-
-    @classmethod
-    def parse(cls, report):
-        def walk():
-            for row in report.rows:
-                if (
-                    row[0].lower() == 'withholding tax' and
-                    row[1].lower() == 'data' and
-                    row[2].lower() != 'total'
-                ):
-                    yield cls(*row)
-
-        return list(walk())
-
-
 def to_row(currencies_map, fee):
     currency_rate = currencies_map[fee.currency][fee.datetime.date()]
 
